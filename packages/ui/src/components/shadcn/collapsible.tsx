3eede14f061fd50c8c722892346c7aeb260cc393
duc thang <thangld180401@gmail.com>
2025-12-02 18:40:51 +0700

    update

--- packages/ui/src/components/shadcn/collapsible.tsx
+++ packages/ui/src/components/shadcn/collapsible.tsx
@@ -9,9 +9,7 @@ function Collapsible({ ...props }: React.ComponentProps<typeof CollapsiblePrimit
   );
 }
 
-function CollapsibleTrigger({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+function CollapsibleTrigger({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
   return (
     <CollapsiblePrimitive.CollapsibleTrigger
       data-slot="collapsible-trigger"
@@ -20,9 +18,7 @@ function CollapsibleTrigger({
   );
 }
 
-function CollapsibleContent({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+function CollapsibleContent({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
   return (
     <CollapsiblePrimitive.CollapsibleContent
       data-slot="collapsible-content"
